c4e5c3ce90064172fe8e459bd3741a2a84159385
Nick Tomlin <nick.tomlin@gmail.com>
2015-12-04 11:05:18 -0600

    Remove scripts

--- lib/scripts/get-token.rb
+++
@@ -1,5 +0,0 @@
-id = Rails.application.secrets.google_client_id
-secret = Rails.application.secrets.google_client_secret
-
-auth = Auth::GoogleOauth.new(id, secret, {callback_url: 'http://localhost:3050/auth/google/callback', scope: 'profile'})
-puts auth.authorize_url
