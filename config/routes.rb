450acfe3b7d670e72c68b7def2e693bc16f445c3
Nick Tomlin <nick.tomlin@gmail.com>
2015-12-04 18:28:38 -0600

    Add developer routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   post 'auth/dangerously_basic_auth' => 'auth#dangerously_basic_auth'
   get 'auth/google/callback' => 'auth_google#callback'
   get 'auth/google/init' => 'auth_google#init'
+  get 'auth/developer/init' => 'auth_developer#init'
+  get 'auth/developer/callback' => 'auth_developer#callback'
 
   namespace 'api' do
     get 'status'
